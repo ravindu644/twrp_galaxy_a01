2af6bca191a5a37109c75866b5b69b0e9b387987
Ravindu Deshan <Droidcasts@pm.me>
2023-03-24 00:15:31 +0530

    Fixed "Incorrect TARGET_ARCH_VARIANT, armeabi-v7a. Use armv8-a instead"

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@ ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armeabi-v7a
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
